5b0e9e482a693bbbe4e904cccdcb49dbea5c8157
Mr Yazdan <mrrezayazdani@yahoo.com>
2021-05-01 16:40:34 +0430

    create and configure main function

--- main.sh
+++ main.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-main(){
+main() {
   clear
+  pyfiglet Phone Book
+  echo ''
+  echo '================= MAIN ================='
+  echo ''
+  echo '  1. Add Phone Number '
+  echo '  2. Show All Phone Number '
+  echo '  0. Exit Program '
+  echo ''
+  echo -n ' >> '
+  read -r mode
+
+  if [ "$mode" -eq 1 ]; then
+    pass
+
+  elif [ "$mode" -eq 2 ]; then
+    pass
+
+  elif [ "$mode" -eq 3 ]; then
+    pass
+
+  else
+    pass
+
+  fi
 }
 
 main
